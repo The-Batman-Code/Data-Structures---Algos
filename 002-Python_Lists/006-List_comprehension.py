86bc707b65006c0c4276343eef452bc4e876a744
Kartik Saxena <mailtokartiksaxena@gmail.com>
2023-12-15 21:01:20 +0530

    using git

---
+++ 002-Python_Lists/006-List_comprehension.py
@@ -0,0 +1,13 @@
+a = [1, 2, 3, 4]
+b = [item * 2 for item in a]
+print(b)
+
+import numpy as np
+
+d = np.array([1, 2, 3, 4])
+e = d * 2
+print(e)
+
+k = "Kartik"
+new_list = [letter for letter in k]
+print(new_list)
